1a211debcf682ca61084ca72e35937efab6bd1b9
guos930112 <46306257+guos930112@users.noreply.github.com>
2019-01-02 11:40:38 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
 def func():
     print("hello world")
+
+def fib(count):
+    a = 0 
+    b = 1 
+    n = 0 
+    while n < count:
+        yield a 
+        a ,b = b ,a+b
+        n += 1
+        
+    
